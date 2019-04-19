5709871118680daeae5de2a18e159b517125b51d
Dmitry Elishevich <delishevich@tula.co>
2019-04-19 22:46:53 +0300

    Step 4. Use interaptions to control speed of model.

--- ServoDriveController.h
+++ ServoDriveController.h
@@ -6,8 +6,96 @@ class servo_drive_controller
 	int move_right_side_back_;
 	int move_left_side_back_;
 	int move_left_side_forward_;
-	const int ShimMax = 255;
-	int CurrentSpeed = 0;
+	int current_right_speed_ = 0;
+	int current_left_speed_ = 0;
+
+	const int speed_max_ = 255;
+	const int speed_change_value_ = 45;
+
+	void moving() const
+	{
+		digitalWrite(move_right_side_forward_, LOW);
+		digitalWrite(move_right_side_back_, LOW);
+		digitalWrite(move_left_side_forward_, LOW);
+		digitalWrite(move_left_side_back_, LOW);
+		if (current_right_speed_ < current_left_speed_)
+		{
+			delay(speed_max_ - current_left_speed_);
+			digitalWrite(move_left_side_forward_, HIGH);
+			delay(current_left_speed_ - current_right_speed_);
+			digitalWrite(move_right_side_forward_, HIGH);
+			delay(current_left_speed_);
+		}
+		else if (current_right_speed_ > current_left_speed_)
+		{
+			delay(speed_max_ - current_right_speed_);
+			digitalWrite(move_right_side_forward_, HIGH);
+			delay(current_right_speed_ - current_left_speed_);
+			digitalWrite(move_left_side_forward_, HIGH);
+			delay(current_right_speed_);
+		}
+		else
+		{
+			delay(speed_max_ - current_right_speed_);
+			digitalWrite(move_right_side_forward_, HIGH);
+			digitalWrite(move_left_side_forward_, HIGH);
+			delay(current_right_speed_);
+		}
+	}
+
+	void change_speed(int expected_right, int expected_left)
+	{
+		if (current_right_speed_ != expected_right)
+		{
+			if (expected_right > current_right_speed_)
+			{
+				if (current_right_speed_ + speed_change_value_ > speed_max_)
+				{
+					current_right_speed_ = speed_max_;
+				}
+				else
+				{
+					current_right_speed_ += speed_change_value_;
+				}
+			}
+			else
+			{
+				if (current_right_speed_ - speed_change_value_ <= 0)
+				{
+					current_right_speed_ = 0;
+				}
+				else
+				{
+					current_right_speed_ -= speed_change_value_;
+				}
+			}
+		}
+		if (current_left_speed_ != expected_left)
+		{
+			if (expected_left > current_left_speed_)
+			{
+				if (current_left_speed_ + speed_change_value_ > speed_max_)
+				{
+					current_left_speed_ = speed_max_;
+				}
+				else
+				{
+					current_left_speed_ += speed_change_value_;
+				}
+			}
+			else
+			{
+				if (current_left_speed_ - speed_change_value_ <= 0)
+				{
+					current_left_speed_ = 0;
+				}
+				else
+				{
+					current_left_speed_ -= speed_change_value_;
+				}
+			}
+		}
+	}
 
 public:
 
@@ -23,42 +111,16 @@ public:
 		pinMode(move_left_side_forward_, OUTPUT);
 	}
 
-	//  ---------------- MOVING -------------------------
-	void move_forward() const
+	void set_speed(int r_speed, int l_speed)
 	{
-		move_stop();
-		digitalWrite(move_right_side_forward_, HIGH);
-		digitalWrite(move_left_side_forward_, HIGH);
+		if (r_speed > speed_max_)
+			r_speed = speed_max_;
+		if (l_speed > speed_max_)
+			l_speed = speed_max_;
+		while (current_left_speed_ != l_speed && current_right_speed_ != r_speed)
+		{
+			change_speed(r_speed, l_speed);
+			moving();
+		}
 	}
-
-	void move_back() const
-	{
-		move_stop();
-		digitalWrite(move_right_side_back_, HIGH);
-		digitalWrite(move_left_side_back_, HIGH);
-	}
-
-	void turn_right() const
-	{
-		move_stop();
-		digitalWrite(move_right_side_back_, HIGH);
-		digitalWrite(move_left_side_forward_, HIGH);
-	}
-
-	void turn_left() const
-	{
-		move_stop();
-		digitalWrite(move_right_side_forward_, HIGH);
-		digitalWrite(move_left_side_back_, HIGH);
-	}
-
-	void move_stop() const
-	{
-		digitalWrite(move_right_side_forward_, LOW);
-		digitalWrite(move_right_side_back_, LOW);
-		digitalWrite(move_left_side_forward_, LOW);
-		digitalWrite(move_left_side_back_, LOW);
-	}
-
-	//  ================= MOVING ========================
 };
--- arduino_car_kit.ino
+++ arduino_car_kit.ino
@@ -1,4 +1,4 @@
-#include "ServoDriveController.h";
+#include "ServoDriveController.h"
 
 servo_drive_controller *drive_ctrl;
 
@@ -11,23 +11,23 @@ void loop()
 {
 	delay(5000);
 
-	drive_ctrl->move_forward();
+	drive_ctrl->set_speed(255, 255);
 	delay(1000);
-	drive_ctrl->move_stop();
-	delay(1000);
-
-	drive_ctrl->move_back();
-	delay(1000);
-	drive_ctrl->move_stop();
+	drive_ctrl->set_speed(155, 155);
 	delay(1000);
 
-	drive_ctrl->turn_right();
+	drive_ctrl->set_speed(155, 55);
 	delay(1000);
-	drive_ctrl->move_stop();
+	drive_ctrl->set_speed(55, 55);
 	delay(1000);
 
-	drive_ctrl->turn_left();
+	drive_ctrl->set_speed(55, 155);
 	delay(1000);
-	drive_ctrl->move_stop();
+	drive_ctrl->set_speed(155, 155);
+	delay(1000);
+
+	drive_ctrl->set_speed(255, 255);
+	delay(1000);
+	drive_ctrl->set_speed(0, 0);
 	delay(1000);
 }
